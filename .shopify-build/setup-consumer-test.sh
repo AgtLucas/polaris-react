33ce63faaa0d0c02b5160fb0d0f04a132f60fe2d
amrocha <andre.rocha@shopify.com>
2020-02-11 21:03:43 -0500

    cat

--- .shopify-build/setup-consumer-test.sh
+++ .shopify-build/setup-consumer-test.sh
@@ -4,7 +4,9 @@ set -e
 
 yarn run build
 yarn pack
+echo $GOOGLE_CLOUD_SERVICE_ACCOUNT
 echo $GOOGLE_CLOUD_SERVICE_ACCOUNT > './gcpKey.json'
+cat ./gcpKey.json
 export GOOGLE_APPLICATION_CREDENTIALS="$(pwd)/gcpKey.json"
 mv shopify-polaris-*.tgz "shopify-polaris-${BUILDKITE_COMMIT}.tgz"
 pwd
