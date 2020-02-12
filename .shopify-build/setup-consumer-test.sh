e10c6acc75c69373a68ce373723442800a39095f
amrocha <andre.rocha@shopify.com>
2020-02-11 20:58:34 -0500

    rename package

--- .shopify-build/setup-consumer-test.sh
+++ .shopify-build/setup-consumer-test.sh
@@ -6,5 +6,6 @@ yarn run build
 yarn pack
 echo $GOOGLE_CLOUD_SERVICE_ACCOUNT > './gcpKey.json'
 export GOOGLE_APPLICATION_CREDENTIALS="$(pwd)gcpKey.json"
+mv shopify-polaris-*.tgz "shopify-polaris-${BUILDKITE_COMMIT}.tgz"
 pwd
-node ./scripts/upload-artifact.js
+node ./scripts/upload-artifact.js "./shopify-polaris-${BUILDKITE_COMMIT}.tgz"
