d72ff1ccf3856fa757988a849842ba6c9ff97e02
amrocha <andre.rocha@shopify.com>
2020-02-11 22:02:18 -0500

    Update env

--- .shopify-build/setup-consumer-test.sh
+++ .shopify-build/setup-consumer-test.sh
@@ -7,5 +7,4 @@ yarn pack
 echo $GOOGLE_CLOUD_SERVICE_ACCOUNT | base64 -d > './gcpKey.json'
 export GOOGLE_APPLICATION_CREDENTIALS="$(pwd)/gcpKey.json"
 mv shopify-polaris-*.tgz "shopify-polaris-${BUILDKITE_COMMIT}.tgz"
-pwd
 node ./scripts/upload-artifact.js "./shopify-polaris-${BUILDKITE_COMMIT}.tgz"
