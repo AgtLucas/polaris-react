35493c70e2c37055174a995224336de6230268d5
amrocha <andre.rocha@shopify.com>
2020-02-11 20:49:20 -0500

    Update path

--- .shopify-build/setup-consumer-test.sh
+++ .shopify-build/setup-consumer-test.sh
@@ -7,4 +7,4 @@ yarn pack
 echo $GOOGLE_CLOUD_SERVICE_ACCOUNT > './gcpKey.json'
 export GOOGLE_APPLICATION_CREDENTIALS="$(pwd)gcpKey.json"
 pwd
-node ../scripts/upload-artifact.js
+node ./scripts/upload-artifact.js
